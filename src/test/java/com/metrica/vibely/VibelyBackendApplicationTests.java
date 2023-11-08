873345ef56c4ffde1ad5c64b2290bc23d872f658
Quathar <99633210+Quathar@users.noreply.github.com>
2023-11-08 11:32:05 +0100

    Initial commit

---
+++ src/test/java/com/metrica/vibely/VibelyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrica.vibely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibelyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
