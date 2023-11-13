433d1b2d6e2f1b1b093bce41dfa7c96c8922bb36
Q <99633210+Quathar@users.noreply.github.com>
2023-11-13 15:39:06 +0100

    Add src for :application module

---
+++ Application/src/main/java/com/metrica/vibely/ApplicationApplication.java
@@ -0,0 +1,13 @@
+package com.metrica.vibely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationApplication.class, args);
+	}
+
+}
