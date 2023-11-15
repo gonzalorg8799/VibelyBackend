397a97a756ca2b872bbb513002dd4fc0591cc973
Raul <Raul@DESKTOP-T5U920N>
2023-11-15 11:09:01 +0100

    adding doc + deleteByUsername method to User repository

--- domain/src/main/java/com/metrica/vibely/data/repository/UserRepository.java
+++ domain/src/main/java/com/metrica/vibely/data/repository/UserRepository.java
@@ -7,8 +7,28 @@ import org.springframework.stereotype.Repository;
 
 import com.metrica.vibely.data.entity.User;
 
+/**
+ * 
+ * 
+ * @since 2023-11-14
+ * @version 1.0
+ */
+
 @Repository
 public interface UserRepository extends JpaRepository<User, java.util.UUID> {
 	
+	/**
+	 * 
+	 * @param username
+	 * @return
+	 * @throws NoSuchElementException
+	 */
 	Optional<User> findByUsername(String username);
+	
+	/**
+	 * 
+	 * @param username
+	 * @throws NoSuchElementException
+	 */
+	void deleteByUsername(String username);
 }
