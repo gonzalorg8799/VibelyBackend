74c27e3d4a028f83c2b8942d8a5c96e826edeb0f
Raul <Raul@DESKTOP-T5U920N>
2023-11-15 11:05:17 +0100

    adding doc + changing return to UserService interface methods

--- domain/src/main/java/com/metrica/vibely/data/service/UserService.java
+++ domain/src/main/java/com/metrica/vibely/data/service/UserService.java
@@ -1,13 +1,44 @@
 package com.metrica.vibely.data.service;
 
-import com.metrica.vibely.data.entity.User;
 import com.metrica.vibely.data.model.dto.UserDTO;
 
+/**
+ * 
+ * 
+ * @since 2023-11-14
+ * @version 1.0
+ */
 public interface UserService {
 	
-	User getByUsername(String username);	
-	User create(UserDTO user); // subject to change
-	User update(UserDTO user); // 
+	/**
+	 * 
+	 * @param username 
+	 * @return UserDTO
+	 * @throws NoSuchElementException
+	 */
+	UserDTO getByUsername(String username);	
+	
+	/**
+	 * 
+	 * @param userDTO
+	 * @return userDTO
+	 * @throws NoSuchElementException
+	 */
+	UserDTO create(UserDTO userDTO); 
+	
+	/**
+	 * 
+	 * @param userDTO
+	 * @return userDTO
+	 * @throws NoSuchElementException
+	 */
+	UserDTO update(UserDTO userDTO);  
+	
+	/**
+	 * 
+	 * @param username
+	 * @throws NoSuchElementException
+	 */
 	void deleteByUsername(String username);
 	
 }
