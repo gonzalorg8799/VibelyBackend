14fc824993460fdd9a849bd7a9e0b258766664d7
Q <99633210+Quathar@users.noreply.github.com>
2023-11-16 14:15:26 +0100

    Update UserEntityTest, rename enumerator fields, add more coverage,

--- domain/src/main/java/com/metrica/vibely/data/entity/User.java
+++ domain/src/main/java/com/metrica/vibely/data/entity/User.java
@@ -2,12 +2,13 @@ package com.metrica.vibely.data.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
 import com.metrica.vibely.data.model.enumerator.PrivacyType;
-import com.metrica.vibely.data.model.enumerator.State;
-import com.metrica.vibely.data.model.enumerator.Status;
+import com.metrica.vibely.data.model.enumerator.UserState;
+import com.metrica.vibely.data.model.enumerator.UserStatus;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -19,9 +20,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 /**
+ * <h1>User Entity</h1>
  * 
  * @since 2023-11-13
  * @version 1.0
+ * @author Adrian, Alex
  */
 @Entity
 public class User {
@@ -31,94 +34,104 @@ public class User {
     // Basic
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(nullable = false)
     private UUID userId;
-    
     private String username;
     private String password;
     private String nickname;
     private String email;
-    
-    @Column(name = "api_key")
-    private String apiKey;
-    
+    @Enumerated(value = EnumType.STRING)
+    private UserState state;
     @Enumerated(value = EnumType.STRING)
     private PrivacyType privacy;
-
+    private Integer logins;
     @Enumerated(value = EnumType.STRING)
-    private Status status;
-    
-    @Enumerated(value = EnumType.STRING)
-    private State state;
-
-	private Integer logins;
-	
-	@Column(name = "blocked_date")
-    private LocalDate blockedDate;
-	
+    private UserStatus status;
 	@Column(name = "last_connection_date")
     private LocalDateTime lastConnDate;
+	@Column(name = "blocked_date")
+	private LocalDate blockedDate;
 
     // Relations
     @OneToMany
     private Set<User> followers;
-
     @OneToMany
     private Set<User> following;
-
     @OneToMany(mappedBy = "postId")
     private Set<Post> posts;
-
     @OneToMany(mappedBy = "chatId")
     private Set<Chat> chats;
 
     // <<-CONSTRUCTORS->>
     public User() {
+        this.setUserId(null);
+//        this.posts = new Post.TreePost();
     }
 
     public User(
-    		UUID userId,
-    		String username,
-    		String password, 
-    		String nickname, 
-    		String email, 
-    		String apiKey, 
-    		Set<User> followers,
-            Set<User> following, 
-            Set<Chat> chats, 
-            Set<Post> posts, 
-            PrivacyType privacy, 
-            Status status, 
-            State state,
-            Integer logins, 
-            LocalDate blockedDate, 
-            LocalDateTime lastConnDate) {
+            UUID userId,
+            String username,
+            String password,
+            String nickname,
+            String email,
+            UserState state,
+            PrivacyType privacy,
+            Integer logins,
+            UserStatus status,
+            LocalDateTime lastConnDate,
+            LocalDate blockedDate,
+            Set<User> followers,
+            Set<User> following,
+            Set<Post> posts,
+            Set<Chat> chats
+     ) {
         this.setUserId(userId);
         this.setUsername(username);
         this.setPassword(password);
         this.setNickname(nickname);
         this.setEmail(email);
-        this.setApiKey(apiKey);
+        this.setState(state);
+        this.setPrivacy(privacy);
+        this.setLogins(logins);
+        this.setStatus(status);
+        this.setLastConnDate(lastConnDate);
+        this.setBlockedDate(blockedDate);
         this.setFollowers(followers);
         this.setFollowing(following);
         this.setPosts(posts);
         this.setChats(chats);
-        this.setPrivacy(privacy);
-        this.setStatus(status);
-        this.setState(state);
-        this.setLogins(logins);
-        this.setBlockedDate(blockedDate);
-        this.setLastConnDate(lastConnDate);
     }
 
+    // <<-METHODS->>
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.userId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        return Objects.equals(this.userId, other.userId);
+    }
+    
     // <<-GETTERS & SETTERS->>
     public UUID getUserId() {
         return userId;
     }
 
     public void setUserId(UUID userId) {
-    	if(userId == null) { this.userId = UUID.randomUUID(); }
-        this.userId = userId;
+    	if (userId == null) {
+    	    this.userId = UUID.randomUUID();
+    	} else {
+            this.userId = userId;
+    	}
+    	// Another way
+    	// this.userId = (userId == null) ? UUID.randomUUID() : userId;
     }
 
     public String getUsername() {
@@ -153,6 +166,74 @@ public class User {
         this.email = email;
     }
 
+    public UserState getState() {
+        return state;
+    }
+
+    public void setState(UserState state) {
+        if (state == null) {
+            this.state = UserState.ENABLED;
+        } else {
+            this.state = state;
+        }
+    }
+
+    public PrivacyType getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(PrivacyType privacy) {
+        if (privacy == null) {
+            this.privacy = PrivacyType.PUBLIC;
+        } else {
+            this.privacy = privacy;
+        }
+    }
+
+    public Integer getLogins() {
+        return logins;
+    }
+
+    public void setLogins(Integer logins) {
+        if (logins == null) {
+            this.logins = 0;
+        } else {
+            this.logins = logins;
+        }
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        if (status == null) {
+            this.status = UserStatus.OFFLINE;
+        } else {
+            this.status = status;   
+        }
+    }
+
+    public LocalDateTime getLastConnDate() {
+        return lastConnDate;
+    }
+
+    public void setLastConnDate(LocalDateTime lastConnDate) {
+        if (lastConnDate == null) {
+            this.lastConnDate = LocalDateTime.now();
+        } else {
+            this.lastConnDate = lastConnDate;   
+        }
+    }
+
+    public LocalDate getBlockedDate() {
+        return blockedDate;
+    }
+
+    public void setBlockedDate(LocalDate blockedDate) {
+        this.blockedDate = blockedDate;
+    }
+
     public Set<User> getFollowers() {
         return followers;
     }
@@ -184,64 +265,5 @@ public class User {
     public void setChats(Set<Chat> chats) {
         this.chats = chats;
     }
-
-    public PrivacyType getPrivacy() {
-        return privacy;
-    }
-
-    public void setPrivacy(PrivacyType privacy) {
-    	if(privacy == null) { this.privacy = PrivacyType.PUBLIC; }
-        this.privacy = privacy;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
     
-    public State getState() {
-		return state;
-	}
-
-	public void setState(State state) {
-		if(state == null) { this.state = State.ENABLED; }
-		this.state = state;
-	}
-
-    public void setStatus(Status status) {
-    	if(status == null) { this.status = Status.OFFLINE; }
-        this.status = status;
-    }
-
-    public Integer getLogins() {
-        return logins;
-    }
-
-    public void setLogins(Integer logins) {
-    	if(logins == null) { this.logins = 0; }
-        this.logins = logins;
-    }
-
-    public LocalDate getBlockedDate() {
-        return blockedDate;
-    }
-
-    public void setBlockedDate(LocalDate blockedDate) {
-        this.blockedDate = blockedDate;
-    }
-
-	public LocalDateTime getLastConnDate() {
-		return lastConnDate;
-	}
-
-	public void setLastConnDate(LocalDateTime lastConnDate) {
-		this.lastConnDate = lastConnDate;
-	}
-
-	public String getApiKey() {
-		return apiKey;
-	}
-
-	public void setApiKey(String apiKey) {
-		this.apiKey = apiKey;
-	}
 }
