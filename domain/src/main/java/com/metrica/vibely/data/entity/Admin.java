dd15be390fb31b53939ecabc34428d43a43f5b64
Q <99633210+Quathar@users.noreply.github.com>
2023-11-16 17:11:49 +0100

    Refactor Admin entity

--- domain/src/main/java/com/metrica/vibely/data/entity/Admin.java
+++ domain/src/main/java/com/metrica/vibely/data/entity/Admin.java
@@ -4,11 +4,24 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.ForeignKey;
 
+/**
+ * <h1>Admin Entity</h1>
+ * 
+ * @since 2023-11-13
+ * @version 1.0
+ * @author Adrian, Alex
+ */
 public class Admin{
 	
+    // <<-FIELDS->>
+    
+    // Relations
 	@OneToOne(optional = false)
-	@JoinColumn(name = "user_id", unique = true, nullable = false, foreignKey = @ForeignKey(name = "fk_admin_user"))
+	@JoinColumn(
+	        name = "user_id",
+	        unique = true,
+	        nullable = false,
+	        foreignKey = @ForeignKey(name = "fk_admin_user"))
 	private User user;
-
     
 }
