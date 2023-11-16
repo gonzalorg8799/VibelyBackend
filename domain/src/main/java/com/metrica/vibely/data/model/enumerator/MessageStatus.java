eac2e3c9147f8406aedce354042f21f365280add
Q <99633210+Quathar@users.noreply.github.com>
2023-11-16 17:05:09 +0100

    Update Message entity & add some new fields

---
+++ domain/src/main/java/com/metrica/vibely/data/model/enumerator/MessageStatus.java
@@ -0,0 +1,14 @@
+package com.metrica.vibely.data.model.enumerator;
+
+/**
+ * <h1>Message Status</h1>
+ * 
+ * @since 2023-11-16
+ * @version
+ * @author Alex
+ */
+public enum MessageStatus {
+
+    PENDING, SENT, READ
+    
+}
