365f9b94472e7f213c7116481884f7aea34a8f40
DaniGWasTaken <daniel.giaffreda123@gmail.com>
2023-11-16 15:31:17 +0100

    Created User Repository tests

---
+++ domain/src/test/java/com/metrica/vibely/data/repository/UserRepositoryTests.java
@@ -0,0 +1,126 @@
+package com.metrica.vibely.data.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.metrica.vibely.data.entity.User;
+import com.metrica.vibely.data.model.enumerator.PrivacyType;
+import com.metrica.vibely.data.model.enumerator.State;
+import com.metrica.vibely.data.model.enumerator.Status;
+import com.metrica.vibely.data.util.PasswordHashing;
+
+/**
+ * <h1>User Entity Test</h1>
+ * 
+ * @since 2023-11-16
+ * @version 1.0
+ * @author Daniel
+ */
+
+public class UserRepositoryTests {
+
+	// <<-CONSTANTS->>
+    private static final String DEFAULT_USERNAME = "jdoe";
+    private static final String DEFAULT_PASSWORD = "jdoe";
+    private static final String DEFAULT_NICKNAME = "John Doe";
+    private static final String DEFAULT_EMAIL    = "johndoe@email.com";
+
+    // <<-FIELD->>
+    private UserRepository userRepository;
+    private User user = initializeUser("jdoe", "jdoe", "Jhon Doe", "johndoe@email.com");
+    
+    // <<-CONSTRUCTOR->>
+    @Autowired
+    public UserRepositoryTests(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+
+    // <<-METHODS->>
+    private User initializeUser(String username, String password, String nickname, String email) {
+    	User user = new User();
+    	user.setUserId		(null);
+    	user.setUsername	(DEFAULT_USERNAME);
+    	user.setPassword	(PasswordHashing.hash(DEFAULT_PASSWORD));
+    	user.setNickname	(DEFAULT_NICKNAME);
+    	user.setEmail   	(DEFAULT_EMAIL);
+    	user.setState       (State.ENABLED);
+        user.setPrivacy     (PrivacyType.PUBLIC);
+        user.setStatus      (Status.ONLINE);
+        user.setLogins      (0);
+        user.setLastConnDate(LocalDateTime.now());
+        user.setBlockedDate (LocalDate.now());
+        user.setFollowers   (new HashSet<>());
+        user.setFollowing   (new HashSet<>());
+        user.setPosts       (new HashSet<>());
+        user.setChats       (new HashSet<>());
+		return user;
+    }
+    
+    @Test
+    @Order(1)
+    void saveFlushDeleteTest() {
+    	User testUser1 = user;
+    	userRepository.saveAndFlush(testUser1);
+    	User testUser2 = initializeUser("test2", "testing2", "Test Account2", "irrelevant@email.com");
+    	User testUser3 = initializeUser("test3", "testing3", "Test Account3", "notimportant@email.com");
+    	User testUser4 = initializeUser("test4", "testing4", "Test Account4", "deletelater@email.com");
+    	Collection<User> collection = new ArrayList<User>();
+    	collection.add(testUser2);
+    	collection.add(testUser3);
+    	collection.add(testUser4);
+    	
+    	userRepository.saveAllAndFlush(collection);
+    	assertNotNull(userRepository.findByUsername(testUser1.getUsername()));
+    	assertNotNull(userRepository.findByUsername(testUser2.getUsername()));
+    	assertNotNull(userRepository.findByUsername(testUser3.getUsername()));
+    	assertNotNull(userRepository.findByUsername(testUser4.getUsername()));
+    	userRepository.deleteByUsername(testUser1.getUsername());
+    	assertNull(userRepository.findByUsername(testUser1.getUsername()));
+    	userRepository.deleteAll(collection);
+    	assertNotNull(userRepository.findByUsername(testUser2.getUsername()));
+    	assertNotNull(userRepository.findByUsername(testUser3.getUsername()));
+    	assertNotNull(userRepository.findByUsername(testUser4.getUsername()));
+    }
+    
+    @Test
+    @Order(2)
+    void findByUsernameTest() {
+    	userRepository.saveAndFlush(user);
+    	
+    	Optional<User> result = userRepository.findByUsername(DEFAULT_USERNAME);
+    	User searchedUser = result.get();
+    	
+    	assertInstanceOf(User.class, result.get());
+    	assertEquals(user.getUserId(),      searchedUser.getUserId());
+        assertEquals(user.getUsername(),    searchedUser.getUsername());
+        assertEquals(user.getPassword(),    searchedUser.getPassword());
+        assertEquals(user.getNickname(),    searchedUser.getNickname());
+        assertEquals(user.getEmail(),       searchedUser.getEmail());
+        assertEquals(user.getState(),       searchedUser.getState());
+        assertEquals(user.getStatus(),      searchedUser.getStatus());
+        assertEquals(user.getLogins(),      searchedUser.getLogins());
+        assertEquals(user.getBlockedDate(), searchedUser.getBlockedDate());
+        assertEquals(user.getFollowers(),   searchedUser.getFollowers());
+        assertEquals(user.getFollowing(),   searchedUser.getFollowing());
+        assertEquals(user.getChats(),       searchedUser.getChats());
+        
+        userRepository.delete(user);
+        assertNull(userRepository.findByUsername(DEFAULT_USERNAME));
+    }
+}
