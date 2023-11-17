b841c90406bcc5d964af0976a4ff24773e8135a5
Gonzalo Rodriguez <Gonzalo Rodriguez@192.168.0.38>
2023-11-17 13:46:23 +0100

    implemented the new InvalidCredentialException for incorrect username or password

--- domain/src/main/java/com/metrica/vibely/data/service/impl/AuthServiceImpl.java
+++ domain/src/main/java/com/metrica/vibely/data/service/impl/AuthServiceImpl.java
@@ -1,13 +1,10 @@
 package com.metrica.vibely.data.service.impl;
 
-import java.security.SecureRandom;
-import java.util.Base64;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.metrica.vibely.data.entity.User;
-import com.metrica.vibely.data.exception.HttpUnauthorizedException;
+import com.metrica.vibely.data.exception.InvalidCredentialsException;
 import com.metrica.vibely.data.repository.UserRepository;
 import com.metrica.vibely.data.service.AuthService;
 import com.metrica.vibely.data.util.ApiKeyGenerator;
@@ -21,28 +18,26 @@ import com.metrica.vibely.data.util.PasswordHashing;
 @Service
 public class AuthServiceImpl implements AuthService {
 
-    // <<-FIELD->>
-    private UserRepository userRepository;
+	// <<-FIELD->>
+	private UserRepository userRepository;
 
-    // <<-CONSTRUCTOR->>
-    @Autowired
-    public AuthServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	// <<-CONSTRUCTOR->>
+	@Autowired
+	public AuthServiceImpl(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-    // <<-METHODS->>
-    @Override
-    public String authenticate(String username, String password) {
-        User user = userRepository.findByUsername(username).orElseThrow();
-        String apiKey = "";
-            if (PasswordHashing.matches(password, user.getPassword())) {
-                user.setLogins(user.getLogins() + 1);
-                apiKey=ApiKeyGenerator.generate();
-                userRepository.save(user);
-            }
-            else throw new HttpUnauthorizedException();
-        
-        return apiKey;
-    }
+	// <<-METHODS->>
+	@Override
+	public String authenticate(String username, String password) {
+		User user = userRepository.findByUsername(username).orElseThrow(() -> new InvalidCredentialsException());
+		if (PasswordHashing.matches(password, user.getPassword())) {
+			user.setLogins(user.getLogins() + 1);
+			userRepository.save(user);
+			return ApiKeyGenerator.generate();
+		} 
+		else throw new InvalidCredentialsException();
+
+	}
 
 }
