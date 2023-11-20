850c2c7b317cc510ca921cce9ab089bcc037c96b
adriancristobal <90755273+adriancristobal@users.noreply.github.com>
2023-11-20 16:58:16 +0100

    Test DTOs

--- domain/src/main/java/com/metrica/vibely/data/entity/Post.java
+++ domain/src/main/java/com/metrica/vibely/data/entity/Post.java
@@ -127,7 +127,7 @@ public class Post implements Copyable<Post> {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) 
             return true;
         if (obj == null)
             return false;
