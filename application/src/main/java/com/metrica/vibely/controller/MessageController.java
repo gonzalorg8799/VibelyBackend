277f95a1470f6d3a817749640ee588ef069a2e5a
gonzalorg879 <gonzalorg8799@gmail.com>
2023-11-20 14:49:25 +0100

    created the MessageController

---
+++ application/src/main/java/com/metrica/vibely/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.metrica.vibely.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @since 2023-11-20
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+//	<<--FIELDS-->>
+	private MessageService messageService;
+//	<<--CONSTRUCTOR-->>
+	public MessageController(MessageService messageService) {
+		this.messageService=messageService;
+	}
+//	<<--METHODS-->>
+
+}
