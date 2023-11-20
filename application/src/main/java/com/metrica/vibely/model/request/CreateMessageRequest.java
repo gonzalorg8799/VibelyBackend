a32878b54830f21806878e991755e85e43a76859
gonzalorg879 <gonzalorg8799@gmail.com>
2023-11-20 14:51:33 +0100

    Created the CreateMessageRequest

---
+++ application/src/main/java/com/metrica/vibely/model/request/CreateMessageRequest.java
@@ -0,0 +1,98 @@
+package com.metrica.vibely.model.request;
+
+import java.time.LocalDateTime;
+
+import com.metrica.vibely.data.entity.Chat;
+import com.metrica.vibely.data.entity.User;
+import com.metrica.vibely.data.model.enumerator.MessageStatus;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * @since 2023-11-20
+ * @version 1.0
+ */
+public class CreateMessageRequest {
+	
+//	<<--FIELDS-->>
+	@NotNull
+	@NotBlank
+	private LocalDateTime creationTimestamp;
+	
+	@NotNull
+	@NotBlank
+	private MessageStatus status;
+	
+	@NotNull
+	@NotBlank
+	private String content;
+	
+	@NotNull
+	@NotBlank
+	private Chat chat;
+	
+	@NotNull
+	@NotBlank
+	private User sender;
+
+//	<<--CONSTRUCTOR-->>
+	public CreateMessageRequest() {
+		
+	}
+
+//	<<--METHODS-->>
+	public static MessageDTO toMessageDTO(CreateMessageRequest message) {
+		MessageDTO messageDto = new MessageDTO();
+		
+		messageDto.setCreationTimestamp(message.getCreationTimestamp());
+		messageDto.setStatus		   (message.getStatus());
+		messageDto.setContent		   (message.getContent());
+		messageDto.setChat			   (message.getChat());
+		messageDto.setSender           (message.getSender());
+		
+		return messageDto;
+	}
+	
+//	<<--GETTERS & SETTERS-->>
+
+	public LocalDateTime getCreationTimestamp() {
+		return creationTimestamp;
+	}
+
+	public void setCreationTimestamp(LocalDateTime creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
+
+	public MessageStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(MessageStatus status) {
+		this.status = status;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Chat getChat() {
+		return chat;
+	}
+
+	public void setChat(Chat chat) {
+		this.chat = chat;
+	}
+
+	public User getSender() {
+		return sender;
+	}
+
+	public void setSender(User sender) {
+		this.sender = sender;
+	}
+}
