0b828cdff745e87c1e933457d9c778a526e173fc
gonzalorg879 <gonzalorg8799@gmail.com>
2023-11-20 14:50:35 +0100

    Created the AdminController

---
+++ application/src/main/java/com/metrica/vibely/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.metrica.vibely.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @since 2023-11-20
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class ChatController {
+	
+//	<<--FIELDS-->
+	private ChatService chatService;
+	
+//	<<--CONSTRUCTOR-->>
+	@Autowired
+	public ChatController(ChatService chatService) {
+		this.chatService=chatService;
+	}
+}
