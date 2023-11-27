bb78cbc3a069aadafaf088fbca8f8a311e472d3d
Raul Valentin Radu <Raul Valentin Radu@192.168.0.107>
2023-11-27 16:53:11 +0100

    creating updatechatrequest

--- application/src/main/java/com/metrica/vibely/controller/ChatController.java
+++ application/src/main/java/com/metrica/vibely/controller/ChatController.java
@@ -5,9 +5,11 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import com.metrica.vibely.data.model.dto.ChatDTO;
 import com.metrica.vibely.data.service.ChatService;
 import com.metrica.vibely.model.request.CreateChatRequest;
 import com.metrica.vibely.model.response.CreateChatResponse;
+import com.metrica.vibely.model.response.UpdateChatRequest;
 
 import jakarta.validation.Valid;
 
@@ -42,13 +45,27 @@ public class ChatController {
 		return chatService.getById(chatId);
 	}
 	
-//	@PutMapping("/{id}")
-//	public ResponseEntity<ChatDTO> updateById(
-//			@RequestBody @Valid 
-//			CreateChatRequest chat, 
-//			BindingResult bindingResult) {
-//		
-//	}
+	@PutMapping("/{id}")
+    public ResponseEntity<?> updateById(
+            @PathVariable
+            UUID id,
+            @RequestBody
+            @Valid
+            UpdateChatRequest chatRequest,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        
+        ChatDTO chatDTO = chatRequest.toDTO();
+        chatDTO.setChatId(id);
+        
+        ChatDTO updatedDTO = this.chatService.update(chatDTO);
+        return ResponseEntity.ok()
+                .body(new CreateChatResponse().generateResponse(updatedDTO));
+    }
+
 	@PostMapping("/newchat")
 	public ResponseEntity<CreateChatResponse> create(
 			@RequestBody
@@ -63,4 +80,11 @@ public class ChatController {
 		return ResponseEntity.ok()
 							.body(new CreateChatResponse().generateResponse(chatDto));
 	}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteByUsername(@PathVariable UUID chatId) {
+        this.chatService.deleteById(chatId);
+        return ResponseEntity.noContent().build();
+    }
+	
 }
