96c05983a6e73091e5a8565ceddaa08aef6fec22
Raul Valentin Radu <Raul Valentin Radu@192.168.0.107>
2023-11-27 16:38:14 +0100

    modifiying chatrequest

--- application/src/main/java/com/metrica/vibely/model/request/CreateChatRequest.java
+++ application/src/main/java/com/metrica/vibely/model/request/CreateChatRequest.java
@@ -1,13 +1,10 @@
 package com.metrica.vibely.model.request;
 
-import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-import com.metrica.vibely.data.entity.Message;
 import com.metrica.vibely.data.entity.User;
 import com.metrica.vibely.data.model.dto.ChatDTO;
-import com.metrica.vibely.data.model.enumerator.ChatStatus;
-import com.metrica.vibely.data.model.enumerator.ChatType;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -20,24 +17,8 @@ public class CreateChatRequest {
 //	<<--FIELDS-->>
 	@NotNull
 	@NotBlank
-	private LocalDateTime creationDate;
-	
-	@NotNull
-	@NotBlank
-	private ChatType type;
-	
-	@NotNull
-	@NotBlank
-	private ChatStatus status;
-	@NotNull
-	@NotBlank
 	private String title;
-	
-	@NotNull
-	@NotBlank
-	private LocalDateTime lastActivity;
 	private Set<User> participants;
-	private Set<Message> messages;
 	
 //	<<--CONSTRUCTOR-->>
 	public CreateChatRequest() {
@@ -45,44 +26,16 @@ public class CreateChatRequest {
 	}
 
 //	<<--METHODS-->>
-	public static ChatDTO toChatDTO(CreateChatRequest chat) {
+	public ChatDTO toChatDTO() {
 		ChatDTO chatDto = new ChatDTO();
 		
-		chatDto.setCreationDate(chat.getCreationDate());
-		chatDto.setType		   (chat.getType());
-		chatDto.setStatus	   (chat.getStatus());
-		chatDto.setTitle	   (chat.getTitle());
-		chatDto.setLastActivity(chat.getLastActivity());
-//		chatDto.setParticipants(chat.getParticipants());
-//		chatDto.setMessages	   (chat.getMessages());
-		
+		chatDto.setTitle	   (this.getTitle());
+		chatDto.setParticipants(this.getParticipants().stream()
+													  .map(p -> p.getUserId())
+													  .collect(Collectors.toSet()));
 		return chatDto;
 	}
 //	<<--GETTERS & SETTERS-->>
-
-	public LocalDateTime getCreationDate() {
-		return creationDate;
-	}
-
-	public void setCreationDate(LocalDateTime creationDate) {
-		this.creationDate = creationDate;
-	}
-
-	public ChatType getType() {
-		return type;
-	}
-
-	public void setType(ChatType type) {
-		this.type = type;
-	}
-
-	public ChatStatus getStatus() {
-		return status;
-	}
-
-	public void setStatus(ChatStatus status) {
-		this.status = status;
-	}
 
 	public String getTitle() {
 		return title;
@@ -90,14 +43,6 @@ public class CreateChatRequest {
 
 	public void setTitle(String title) {
 		this.title = title;
-	}
-
-	public LocalDateTime getLastActivity() {
-		return lastActivity;
-	}
-
-	public void setLastActivity(LocalDateTime lastActivity) {
-		this.lastActivity = lastActivity;
 	}
 
 	public Set<User> getParticipants() {
@@ -108,12 +53,5 @@ public class CreateChatRequest {
 		this.participants = participants;
 	}
 
-	public Set<Message> getMessages() {
-		return messages;
-	}
-
-	public void setMessages(Set<Message> messages) {
-		this.messages = messages;
-	}
 	
 }
