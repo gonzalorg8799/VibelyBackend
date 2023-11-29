5980077bd8c1d5af4eb743feee90ce6f4681c2c7
Raul <Raul@DESKTOP-T5U920N>
2023-11-29 12:16:03 +0100

    creating updateresponse

---
+++ application/src/main/java/com/metrica/vibely/model/response/UpdateChatResponse.java
@@ -0,0 +1,51 @@
+package com.metrica.vibely.model.response;
+
+import com.metrica.vibely.data.model.dto.ChatDTO;
+import com.metrica.vibely.data.model.enumerator.ChatStatus;
+import com.metrica.vibely.data.model.enumerator.ChatType;
+
+public class UpdateChatResponse {
+	// ATRIBUTES
+	private String title;
+	private ChatStatus status;
+	private ChatType type;
+	
+	public UpdateChatResponse() {
+		super();
+	}
+	
+	public UpdateChatResponse generateResponse(ChatDTO chatDto) {
+		
+		this.setTitle(chatDto.getTitle());
+		this.setStatus(chatDto.getStatus());
+		this.setType(chatDto.getType());
+		
+		return this;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public ChatStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(ChatStatus status) {
+		this.status = status;
+	}
+
+	public ChatType getType() {
+		return type;
+	}
+
+	public void setType(ChatType type) {
+		this.type = type;
+	}
+	
+	
+}
