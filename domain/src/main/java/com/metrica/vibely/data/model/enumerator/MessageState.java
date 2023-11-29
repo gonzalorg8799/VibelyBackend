14e4dd5f4f893edeeec82069ceb6ea87f4d5ddde
gonzalorg879 <gonzalorg8799@gmail.com>
2023-11-29 11:29:36 +0100

    Created the MessageState enumerator

---
+++ domain/src/main/java/com/metrica/vibely/data/model/enumerator/MessageState.java
@@ -0,0 +1,14 @@
+package com.metrica.vibely.data.model.enumerator;
+/**
+ * <h1>Message State</h1>
+ * 
+ * @since 2023-11-16
+ * @version
+ * @author Gonzalo
+ */
+
+public enum MessageState {
+	
+	ENABLED, DISABLED
+	
+}
