f0ff5f03d6a73676210f8844e1ecacb3f0e6b010
Q <99633210+Quathar@users.noreply.github.com>
2023-11-29 10:56:09 +0100

    Generate ApiKeyGenerator test

--- domain/src/main/java/com/metrica/vibely/data/util/ApiKeyGenerator.java
+++ domain/src/main/java/com/metrica/vibely/data/util/ApiKeyGenerator.java
@@ -1,16 +1,61 @@
 package com.metrica.vibely.data.util;
 
+import java.nio.ByteBuffer;
 import java.security.SecureRandom;
+import java.time.Instant;
 import java.util.Base64;
 
-public class ApiKeyGenerator {
-	public static String generate() {
-        // Genera una clave aleatoria utilizando SecureRandom 
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] keyBytes = new byte[32];
-        secureRandom.nextBytes(keyBytes);
-        // Codifica la clave en Base64 para obtener una representación de cadena
-        String apiKey = Base64.getEncoder().encodeToString(keyBytes);
-        return apiKey;
+/**
+ * <h1>API Key Generator</h1>
+ * 
+ * @since 2023-11-20
+ * @version 1.0
+ * @author Gonzalo, Alex
+ */
+public abstract class ApiKeyGenerator {
+
+    // <<-CONSTANTS->>
+    private static final int KEY_SIZE_BYTES = 32;
+    private static final int LONG_SIZE_BYTES = Long.BYTES;
+
+    // <<-METHODS->>
+    /**
+     * Generates a random API key using a secure random number generator.
+     * 
+     * @return a randomly generated API key as a Base64-encoded string.
+     */
+    public static String generate() {
+        byte[] keyBytes = new byte[KEY_SIZE_BYTES];
+        new SecureRandom().nextBytes(keyBytes);
+
+        // Get current time and set expiration time (1hour)
+        long currentTime = Instant.now().getEpochSecond();
+        long expirationTime = currentTime + 3600;
+
+        ByteBuffer buffer = ByteBuffer.allocate(KEY_SIZE_BYTES + LONG_SIZE_BYTES);
+        buffer.put(keyBytes);
+        buffer.putLong(expirationTime);
+
+        byte[] concatenated = buffer.array();
+        return Base64.getEncoder().encodeToString(concatenated);
     }
+    
+    /**
+     * Retrieves the expiration time from the provided API key.
+     *
+     * @param apiKey The API key containing expiration and creation time.
+     * @return the expiration time in seconds since the epoch.
+     * @throws IllegalArgumentException if the provided API key is invalid.
+     */
+    public static long getExpirationTime(String apiKey) {
+        byte[] concatenated = Base64.getDecoder().decode(apiKey);
+        
+        if (concatenated.length < (KEY_SIZE_BYTES + LONG_SIZE_BYTES)) {
+            throw new IllegalArgumentException("Invalid API key format");
+        }
+        
+        ByteBuffer buffer = ByteBuffer.wrap(concatenated, KEY_SIZE_BYTES, LONG_SIZE_BYTES);
+        return buffer.getLong();
+    }
+
 }
