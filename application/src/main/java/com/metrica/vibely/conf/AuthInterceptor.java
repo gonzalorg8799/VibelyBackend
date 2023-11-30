f0af6bf1909d1915e47a9a5f0c484c0761aefeaa
gonzalorg879 <gonzalorg8799@gmail.com>
2023-11-30 16:15:43 +0100

    Changes in AuthInterceptor

--- application/src/main/java/com/metrica/vibely/conf/AuthInterceptor.java
+++ application/src/main/java/com/metrica/vibely/conf/AuthInterceptor.java
@@ -1,22 +1,49 @@
 package com.metrica.vibely.conf;
 
-import com.metrica.vibely.data.util.ApiKeyManager;
+import java.util.UUID;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.metrica.vibely.data.service.UserService;
+import com.metrica.vibely.data.util.ApiKeyManager;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * 
  */
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
+	
+	private UserService userService;
+	private String savedApiKey;
     
+	@Autowired
+	public AuthInterceptor(UserService userService) {
+		this.userService= userService;
+	}
     // <<-METHODS->>
+	private boolean getStatus(String apiKey, HttpServletResponse response, String savedApiKey) {
+		return switch(ApiKeyManager.isValid(apiKey, savedApiKey)) {
+			case 0->true;
+			case 1->{
+	        	response.setStatus(400);
+	        	yield false;}
+	        case 2->{
+	        	response.setStatus(401);
+	        	yield false;}
+	        default-> false;
+		};
+	}
+	public void getSavedApikey(String apiKey) {
+		UUID id=ApiKeyManager.getId(apiKey);
+		this.savedApiKey=userService.getById(id).getApikey();
+	}
     @Override
     public boolean preHandle(
             @NonNull
@@ -27,14 +54,8 @@ public class AuthInterceptor implements HandlerInterceptor {
             Object handler
     ) {
         String apiKey = request.getHeader("x-api-key");
-
-        ApiKeyManager.getExpirationTime(apiKey);
-
-        System.err.println("Estamos en el interceptor");
-        
-        
-        
-        return false;
+        getSavedApikey(apiKey);
+        return getStatus(apiKey, response, savedApiKey);
     }
 
     @Override
