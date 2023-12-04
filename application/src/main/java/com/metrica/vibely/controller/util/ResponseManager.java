c1b2ae4b6dd0bc404ff508ab24b5234391bfb15f
Raul Valentin Radu <Raul Valentin Radu@192.168.0.107>
2023-12-04 16:17:23 +0100

    creating http status enumerator

--- application/src/main/java/com/metrica/vibely/controller/util/ResponseManager.java
+++ application/src/main/java/com/metrica/vibely/controller/util/ResponseManager.java
@@ -1,7 +1,6 @@
 package com.metrica.vibely.controller.util;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
